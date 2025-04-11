d01a5cb9ad86a7c65f3f882b21da463cc0c545b5
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-11 10:53:28 +0530

    created headers for request call in dio helper class

--- lib/network_manager/dio_helper.dart
+++ lib/network_manager/dio_helper.dart
@@ -1,13 +1,18 @@
 import 'package:dio/dio.dart';
 
 class DioHelper {
+  // Created dio instance
   Dio dio = Dio();
 
+// Created Options for Dio
   Options options = Options(
     receiveDataWhenStatusError: true,
     contentType: 'application/json',
     sendTimeout: const Duration(seconds: 10),
     receiveTimeout: const Duration(seconds: 30),
   );
+
+  // Created Headers for request call
+  Map<String,dynamic> headers = {"Authentication":"Bearer nb37bgh2378rbhvbds23vrl2f67gfwhe vwuy32r6yvu fewyud8f663y"};
 
 }
