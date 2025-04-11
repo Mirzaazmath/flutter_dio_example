cfc46b3b6acb5bcc817cf2efa36c1fc0cdbabf5d
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-11 12:24:30 +0530

    added delete call in dio helper

--- lib/network_manager/dio_helper.dart
+++ lib/network_manager/dio_helper.dart
@@ -121,4 +121,31 @@ class DioHelper {
       return null;
     }
   }
+  // Create a method for delete api call  with parameters like url and bool value to add token and requestBody
+  Future<dynamic> delete({
+    required String url,
+    Object? requestBody,
+    bool isAuthRequired = false,
+  }) async {
+    //  isAuthRequired == true
+    if (isAuthRequired) {
+      // we are adding headers with token
+      options.headers = headers;
+    }
+    try {
+      Response response;
+      if (requestBody == null) {
+        // here we are calling the api with no request body
+        response = await dio.delete(url, options: options);
+      } else {
+        // here we are calling the api with request body
+        response = await dio.delete(url, options: options, data: requestBody);
+      }
+
+      return response.data;
+    } catch (error) {
+      // here we are catching the error
+      return null;
+    }
+  }
 }
