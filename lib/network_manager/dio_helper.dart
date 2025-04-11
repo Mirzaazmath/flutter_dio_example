94decef982a7f22be3a5657024b69219c6e4fb59
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-11 10:49:56 +0530

    created option for dio in diohelper class

--- lib/network_manager/dio_helper.dart
+++ lib/network_manager/dio_helper.dart
@@ -2,4 +2,12 @@ import 'package:dio/dio.dart';
 
 class DioHelper {
   Dio dio = Dio();
+
+  Options options = Options(
+    receiveDataWhenStatusError: true,
+    contentType: 'application/json',
+    sendTimeout: const Duration(seconds: 10),
+    receiveTimeout: const Duration(seconds: 30),
+  );
+
 }
