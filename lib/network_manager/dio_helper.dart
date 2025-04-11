6bd43768de96cc0894c4cebcba96ebbdac4c6dea
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-11 11:03:37 +0530

    added get call method in dio helper class for get call

--- lib/network_manager/dio_helper.dart
+++ lib/network_manager/dio_helper.dart
@@ -4,7 +4,7 @@ class DioHelper {
   // Created dio instance
   Dio dio = Dio();
 
-// Created Options for Dio
+  // Created Options for Dio
   Options options = Options(
     receiveDataWhenStatusError: true,
     contentType: 'application/json',
@@ -13,6 +13,28 @@ class DioHelper {
   );
 
   // Created Headers for request call
-  Map<String,dynamic> headers = {"Authentication":"Bearer nb37bgh2378rbhvbds23vrl2f67gfwhe vwuy32r6yvu fewyud8f663y"};
+  Map<String, dynamic> headers = {
+    "Authentication":
+        "Bearer nb37bgh2378rbhvbds23vrl2f67gfwhe vwuy32r6yvu fewyud8f663y",
+  };
 
+  // Create a method for get api call  with parameters like url and bool value to add token
+  Future<dynamic> get({
+    required String url,
+    bool isAuthRequired = false,
+  }) async {
+    //  isAuthRequired == true
+    if (isAuthRequired) {
+      // we are adding headers with token
+      options.headers = headers;
+    }
+    try {
+      // here we are calling the api
+      Response response = await dio.get(url, options: options);
+      return response.data;
+    } catch (error) {
+      // here we are catching the error
+      return null;
+    }
+  }
 }
